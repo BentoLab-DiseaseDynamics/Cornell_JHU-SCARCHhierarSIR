97109e112622091b3032e792f5dc76516590ecac
twallema <twa27@cornell.edu>
2026-07-31 09:31:31 -0400

    finish job script

--- scripts/operational/submit_hierarchical_training.sh
+++ scripts/operational/submit_hierarchical_training.sh
@@ -2,8 +2,9 @@
 #SBATCH --job-name=my_gpu_job
 #SBATCH --account=arb24_0001
 #SBATCH --partition=cac_cpu
-#SBATCH --time=1:00:00
-#SBATCH -c 8
+#SBATCH --time=24:00:00
+#SBATCH --qos=longrun
+#SBATCH -c 16
 
 # Submit as follows:
 # sbatch submit_hierarchical_training.sh
@@ -12,7 +13,10 @@
 module load anaconda3
 
 # Activate the virtual environment
+source /opt/ohpc/pub/software/anaconda3/etc/profile.d/conda.sh
 conda activate BENTOLAB-SCARCH_HIERARSIR
+unset PYTHONHOME
+unset PYTHONPATH
 
 # Run your Python script
 python hierarchical_training.py
