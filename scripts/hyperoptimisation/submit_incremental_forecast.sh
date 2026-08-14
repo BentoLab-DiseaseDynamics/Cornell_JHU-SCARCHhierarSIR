ed6a665e1fe4ea6b4284886c93747323b629cf2c
twallema <twa27@cornell.edu>
2026-08-14 17:16:30 -0400

    add incremental calibration scripts

---
+++ scripts/hyperoptimisation/submit_incremental_forecast.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name=my_gpu_job
+#SBATCH --account=arb24_0001
+#SBATCH --partition=cac_cpu
+#SBATCH --time=24:00:00
+#SBATCH --qos=longrun
+#SBATCH -c 16
+
+# Submit as follows:
+# sbatch submit_hierarchical_training.sh
+
+# Load any necessary modules
+module load anaconda3
+
+# Activate the virtual environment
+source /opt/ohpc/pub/software/anaconda3/etc/profile.d/conda.sh
+conda activate BENTOLAB-SCARCH_HIERARSIR
+unset PYTHONHOME
+unset PYTHONPATH
+
+# Run your Python script
+python incremental_forecast.py
+
+# Deactivate the virtual environment after the run
+conda deactivate
