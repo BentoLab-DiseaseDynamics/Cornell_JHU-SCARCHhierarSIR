1db832935d3171768c1a34ca5ab7bdd306df505a
twallema <twa27@cornell.edu>
2026-09-11 16:53:08 -0400

    raise defaults

--- scripts/operational/hipergator-submit_training.sh
+++ scripts/operational/hipergator-submit_training.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=training
 #SBATCH --account=epi
 #SBATCH --qos=epi
-#SBATCH --time=01:00:00
+#SBATCH --time=24:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
