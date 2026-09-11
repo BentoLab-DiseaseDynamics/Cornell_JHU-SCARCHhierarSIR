73cd0bc529610b855baa79e3a7d44292c3e25e5e
twallema <twa27@cornell.edu>
2026-09-11 08:47:42 -0400

    hipergator submit

--- scripts/operational/hipergator-submit_training.sh
+++ scripts/operational/hipergator-submit_training.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=training
-#SBATCH --account=cmavian
-#SBATCH --partition=hpg-default
+#SBATCH --account=epi
+#SBATCH --qos=epi
 #SBATCH --time=01:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
